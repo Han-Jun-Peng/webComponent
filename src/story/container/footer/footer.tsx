8e05e1542a5789f6983fcaf80ad1a352a8a2958d
韩俊鹏 <HanJunpeng@aliyun.com>
2023-09-20 17:56:10 +0800

    🦄 refactor(footer.tsx footer.story.tsx):

--- src/story/container/footer/footer.tsx
+++ src/story/container/footer/footer.tsx
@@ -3,27 +3,32 @@ import React, { ReactNode, memo } from "react";
 interface FooterProp {
 	children?: ReactNode;
 	border?: boolean;
+	padding?: boolean;
 	shadow?: boolean;
 }
 
 export const WBFooter = memo(function WBFooter({
 	children,
 	border = false,
+	padding = false,
 	shadow = false,
 }: FooterProp) {
-	const className = [
-		"block",
-		"w-full",
-		"rounded",
-		"px-4 py-2",
-		"bg-black-1 dark:bg-white-1",
-	];
+	const className = ["rounded", "bg-black-1 dark:bg-white-1"];
 
 	if (border) {
 		if (typeof border !== "boolean") {
 			throw new Event(`不能将${typeof border}类型, 赋值给boolean类型的border`);
 		}
 		className.push("border border-black-3 dark:border-white-3");
+	}
+
+	if (padding) {
+		if (typeof padding !== "boolean") {
+			throw new Event(
+				`不能将${typeof padding}类型, 赋值给boolean类型的padding`,
+			);
+		}
+		className.push("px-4 py-2");
 	}
 
 	if (shadow) {
